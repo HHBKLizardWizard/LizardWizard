0ba909c19d7db9b4e118f2cffbcf26989e9224da
Bluey <Green.Lake2812>
2017-06-23 11:31:19 +0200

    SQL

--- src/main/java/repositories/TemplateRepository.java
+++ src/main/java/repositories/TemplateRepository.java
@@ -1,7 +1,15 @@
 package repositories;
 
+import util.DatabaseConnector;
+
+import java.sql.Connection;
+
 /**
  * Created by iho on 22.06.2017.
  */
-public class TemplateRepository {
+public class TemplateRepository implements ITemplateRepository
+{
+    Connection con = new DatabaseConnector().getConnection();
+
+
 }
--- src/main/java/util/DatabaseConnector.java
+++ src/main/java/util/DatabaseConnector.java
@@ -3,23 +3,19 @@ package util;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import javax.sql.DataSource;
-import java.io.PrintWriter;
 import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.SQLFeatureNotSupportedException;
-import java.util.logging.Logger;
 
 /**
  * Created by iho on 22.06.2017.
  */
 public class DatabaseConnector {
-
+    public Connection con = null;
     private static final String driverClassName = "com.mysql.jdbc.Driver";
-    private static final String url = "jdbc:mysql://localhost/";
+    private static final String url = "jdbc:mysql://localhost:3306/user_template_db?serverTimezone=UTC";
     private static final String dbUsername = "root";
     private static final String dbPassword = "";
 
-    public DataSource getDataSource() {
+    private DataSource getDataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName(driverClassName);
         dataSource.setUsername(dbUsername);
@@ -30,14 +26,12 @@ public class DatabaseConnector {
     }
 
     public Connection getConnection() {
-        Connection con = null;
 
         try {
-            con = this.getDataSource().getConnection();
+            this.con = this.getDataSource().getConnection();
         } catch (Exception e) {
             System.out.println(e);
         }
-
         return con;
     }
 }
