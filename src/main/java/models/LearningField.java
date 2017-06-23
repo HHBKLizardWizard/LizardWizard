5f7c94906c39ca710ba3b466e7e7217082daed4f
Waddex <waldemarwerba@gmail.com>
2017-06-23 08:34:37 +0200

    Added LearningField Model

---
+++ src/main/java/models/LearningField.java
@@ -0,0 +1,34 @@
+package models;
+
+/**
+ * Created by Administrator on 23.06.2017.
+ */
+public class LearningField
+{
+    Integer LFID;
+    Integer LFVerweis;
+    String Bezeichnung;
+    Integer LSVerweis;
+    Integer ASVerweis;
+    Integer LFVortitel;
+    Integer LFNR;
+    Integer PraefixID;
+    String PraefixName;
+    Integer ID_Beruffach;
+    Integer LFDauer;
+    Integer Start;
+    Integer Ende;
+    Integer Position;
+    Integer ASPosition;
+    String Ziel;
+    String Wissen;
+    String Fertig;
+    String Sozial;
+    String Selbst;
+    String LFErsteller;
+    String LFGeaender_durch;
+    String LFErstellt_am;
+    String LFGeaendert_am;
+    Boolean Deletet;
+    Boolean Sichtbar;
+}
